264256d0b1b1059011b511aad75a0d7d07ae122b
GREEN♣ <fernando.gabriel.pedro1310@gmail.com>
2021-11-07 15:47:33 -0300

    Formatação Programa1
    Formatação do Programa1 alterada, deixando-a mais organizada e mais semelhante à formatação utilizada no Programa2.

--- Programa1.py
+++ Programa1.py
@@ -1,6 +1,9 @@
 #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor.
-x = int(input("Digite um número inteiro.\n"))
+
+x = float(input("Digite um número inteiro.\n"))
+
 ant, suc = x-1, x+1
-txt = "O antecessor desse número é {}\nO sucessor desse número é {}"
+
+txt = "O antecessor desse número é {}\nO sucessor desse número é {}\n"
 print(txt.format(ant, suc))
 input("Pressione ENTER para encerrar.")
